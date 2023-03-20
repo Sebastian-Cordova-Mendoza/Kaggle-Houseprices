ab7f8932c77d6b79aa80364b2ad37c590739a6fc
Sebastian-Cordova-Mendoza <sebaswn1@gmail.com>
2023-03-20 20:28:34 +0100

    Análisis variables M1

--- rsc/utils/funciones_limpieza.py
+++ rsc/utils/funciones_limpieza.py
@@ -6,24 +6,38 @@ warnings.filterwarnings('ignore')
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 
-#Función que realiza el labelencoder más simple. Se le pasa la columna a codificar y devuelve la columna modificada
+
 def labelencoder_simple(column):
-    
+    """
+    Función que realiza el labelencoder más simple.
+    Se le pasa la columna a codificar y devuelve la columna modificada.
+    """
+
     le = preprocessing.LabelEncoder()
     le.fit(column)
     result = le.transform(column)
 
     return result
 
-#Funicón que realiza OneHotEncoder. Columna a codificar como argumento y 
-def OneHotEncoder_simple(column_df):
 
+
+def OneHotEncoder_simple(df, column_df):
+    """
+    Función que realiza OneHotEncoder. Argumentos: df y la columna a codificar. 
+    Devuelve el df con la columna codificada y la columna origen borrada.
+    Las nuevas columnas añadidas son renombradas con el nombre original de la variable y una etiqueta numérica (n variables codificadas)
+    """
+    
     one_hot_encoder = OneHotEncoder(sparse=False)
-    result = pd.DataFrame(one_hot_encoder.fit_transform(column_df[["LotShape"]].values.reshape(-1,1)))
+    result = pd.DataFrame(one_hot_encoder.fit_transform(column_df.values.reshape(-1,1)))
     renombre = []
+
     for column in result.columns:
         string = column_df.columns[0] + "_" + str(column)
         renombre.append(string)
+
     result.set_axis(renombre, axis=1,inplace=True)
-    
-    return result
+
+    df = df.join(result)
+    del(df[column_df.columns[0]]) 
+    return df
